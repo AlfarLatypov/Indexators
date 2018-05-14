38d9ee7134dfee1342788751d17f9f87649ec32e
ЛатыповА <@223_9>
2018-05-14 16:17:21 +0300

    Add project files.

---
+++ Indexators/Indexer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexators
+{
+    class Indexer
+    {
+
+       public int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+
+        public int this[int ind]
+        {
+
+            get
+            {
+                return arr[ind];
+            }
+            set
+            {
+                arr[ind] = (int)Math.Pow(value, 2);
+            }
+        }
+
+    }
+}
---
+++ Indexators/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Тема: Индексаторы и свойства.
+1.	Создать индексатор для одномерного массива который при установке значения будет возводить в квадрат передаваемое значение переменной 
+и устанавливать его для указанного индекса. При получении элемента массива по индексу будет возвращаться его текущее значение.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Indexer indexer = new Indexer();
+
+            for (int i = 0; i < indexer.arr.Length; i++)
+            {
+                Console.WriteLine($"\tIndex {i} Value {indexer.arr[i]}");
+            }
+
+            Console.WriteLine("\n");
+
+            indexer[0] = 6;
+            indexer[1] = 7;
+            indexer[2] = 8;
+            indexer[3] = 9;
+            indexer[4] = 10;
+
+
+            for (int i = 0; i < indexer.arr.Length; i++)
+            {
+                Console.WriteLine($"\tIndex {i} Value {indexer.arr[i]}");
+            }
+
+
+        }
+    }
+}
